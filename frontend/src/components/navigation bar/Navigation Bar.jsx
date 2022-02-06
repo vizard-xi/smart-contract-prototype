484f453df95ae8ade5f66be0e199fae865ca5536
vizard-xi <kelvin.prince.336@gmail.com>
2022-02-06 22:47:55 +0000

    Built Navigation Bar

---
+++ frontend/src/components/navigation bar/Navigation Bar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import LogoAndWebsiteName from './navigation bar components/Logo and Website Name';
+import NavigationBarMenu from './navigation bar components/Navigation Bar Menu';
+
+class NavigationBar extends Component {
+    
+    render() {
+
+        return (
+
+            <nav className='flex space-x-4 items-stretch  justify-start bg-blue-200 border-2 border-b-black pt-0 mt-0'>
+                <LogoAndWebsiteName />
+                <NavigationBarMenu />       
+            </nav>
+        );
+    }
+}
+
+export default NavigationBar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavigationBar from './components/navigation bar/Navigation Bar';
 
 ReactDOM.render(
   <React.StrictMode>
